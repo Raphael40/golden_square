e9d2185811d3d57bc68b270b4d58fc0b5638075f
Julian Festing <julianfesting@MASTUDENT-Julian-Festing-C02G573RQ6L4.local>
2023-04-13 16:47:23 +0100

    finished punctuation spec

--- phase_2/design_step3/lib/punctuation_checker.rb
+++ phase_2/design_step3/lib/punctuation_checker.rb
@@ -1,3 +1,13 @@
-def punctuation_checker(string)
-  false
+def punctuation_checker(sentence)
+  if sentence == ''
+    false
+  elsif sentence[0] == sentence[0].upcase && sentence[-1] == '.'
+    true
+  elsif sentence[0] == sentence[0].upcase && sentence[-1] == '?'
+    true
+  elsif sentence[0] == sentence[0].upcase && sentence[-1] == '!'
+    true
+  else 
+    false
+  end
 end 
--- phase_2/design_step3/spec/punctuation_checker_spec.rb
+++ phase_2/design_step3/spec/punctuation_checker_spec.rb
@@ -1,12 +1,48 @@
 require 'punctuation_checker'
 
 RSpec.describe 'a method that verifies sentence opens and closes correctly' do 
-  it "returns false when given empty string" do 
-    result = punctuation_checker("")
-    expect(result).to eq false
-  end 
-  it "returns true when given any string" do 
-    result = punctuation_checker("hey there")
-    expect(result).to eq true 
+  context 'returns false' do
+    it "returns false when given empty string" do 
+      result = punctuation_checker('')
+
+      expect(result).to eq false
+    end 
+    it 'returns false when given an uncapitalized string with invalid ending' do 
+      result = punctuation_checker('hey there buddy')
+
+      expect(result).to eq false
+    end
+    it 'returns false when caps is present but no ending punctuation' do
+      result = punctuation_checker('Hey there buddy')
+
+      expect(result).to eq false
+    end
+    it 'returns false with valid ending but no caps' do
+      result = punctuation_checker('hey there buddy.')
+      result_2 = punctuation_checker('hey there buddy!')
+      result_3 = punctuation_checker('hey there buddy?')
+
+      expect(result).to eq false
+      expect(result_2).to eq false
+      expect(result_3).to eq false
+    end
+  end
+
+  context 'returns true for caps combined with ., !, ?' do
+    it 'returns true for caps with .' do
+      result = punctuation_checker('Hey there buddy.')
+
+      expect(result).to eq true
+    end
+    it 'returns true for caps with ?' do
+      result = punctuation_checker('Hey there buddy?')
+
+      expect(result).to eq true
+    end
+    it 'returns true for caps with !' do
+      result = punctuation_checker('Hey there buddy!')
+
+      expect(result).to eq true
+    end
   end
 end 
