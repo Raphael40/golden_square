6b3767af90110c0b2665e0146c6122a2f769d461
Julian Festing <julianfesting@MASTUDENT-Julian-Festing-C02G573RQ6L4.local>
2023-04-13 11:28:21 +0100

    finished make_snipped method

--- phase_two/lib/make_snippet.rb
+++ phase_two/lib/make_snippet.rb
@@ -1,5 +1,12 @@
 def make_snippet(str)
-  new_str = str.split(' ')
+  fail 'Please enter a string' if str.empty?
 
-  new_str.slice(0, 5).join(' ')
+  new_str = str.split(' ')
+  
+  if new_str.length <= 5 && new_str.length >= 1
+    str
+  else
+    new_str = new_str.slice(0, 5).join(' ')
+    "#{new_str}..."
+  end
 end
--- phase_two/spec/make_snippet_spec.rb
+++ phase_two/spec/make_snippet_spec.rb
@@ -1,15 +1,24 @@
 require './lib/make_snippet'
 
 RSpec.describe 'takes a sentence as a string and returns the first five words and a ...' do
-  xit 'takes a string and returns the same string' do
-    result = make_snippet('I am a little string')
 
-    expect(result).to eq 'I am a little string'
+  context 'passing tests' do
+    it 'returns the first five words without a ... if there is less than 5 words' do
+      result = make_snippet('I am tiny')
+
+      expect(result).to eq 'I am tiny'
+    end
+
+    it 'takes a string and returns the first five words followed by ... if more than 5 words' do
+      result = make_snippet('I am a medium sized string')
+
+      expect(result).to eq 'I am a medium sized...'
+    end
   end
 
-  xit 'takes a string and returns the first five words' do
-    result = make_snippet('I am a medium sized string')
-
-    expect(result).to eq 'I am a medium sized'
+  context 'failing tests' do
+    it 'flags an error if empty' do 
+      expect { make_snippet('') }.to raise_error 'Please enter a string'
+    end
   end
 end
