737b7fe461d28cf89afdab28d26fd7fb4c931ae6
Julian Festing <julfesting@gmail.com>
2023-04-20 14:11:25 +0100

    finished wpm method

--- phase_2/design_multi_class/lib/diary.rb
+++ phase_2/design_multi_class/lib/diary.rb
@@ -16,10 +16,32 @@ class Diary
     @entries
   end
 
-  def diary_entries_based_speed(minutes, wpm)
+  def diary_entries_based_on_speed(minutes, wpm)
+    words_we_can_read = minutes * wpm
+    best_fit = []
+    
+    @entries.each do |entry| 
+      if entry.split.length == words_we_can_read
+        best_fit << entry
+      end
+    end
+    
+    if best_fit.empty? 
+      fail "There are no suitable diary entries."
+    else
+      return best_fit
+    end 
   end
   
   def list_mobile_numbers
-    # if an entry is 11 numbers then throw into mobile_numbers array
+
+    mobile_number = @entries
+    
+    mobile_number.map! do |entry|
+      entry.scan(/\d+/)
+    end
+
+    @mobile_numbers << mobile_number
+    return @mobile_numbers.flatten
   end
 end
--- phase_2/design_multi_class/spec/diary_spec.rb
+++ phase_2/design_multi_class/spec/diary_spec.rb
@@ -7,4 +7,42 @@ RSpec.describe do
 
     expect(diary.list_entries).to eq ["my entry"]
   end
+
+  context 'filters phone numbers from diary entires' do
+    it 'stores and lists a number when a diary entry has a number' do
+      diary = Diary.new
+      diary.add_entry("aphex 07927829971 twin")
+      expect(diary.list_mobile_numbers).to eq ["07927829971"]
+    end
+
+    it 'stores and lists a number when a diary entry has a number' do
+      diary = Diary.new
+      diary.add_entry("aphex 07927829971 twin 0728489329846")
+      expect(diary.list_mobile_numbers).to eq ["07927829971", "0728489329846"]
+    end
+
+    it 'takes two number in two entries and returns both' do
+      diary = Diary.new
+      diary.add_entry("aphex 07927829971")
+      diary.add_entry("twin 07927829972")
+      expect(diary.list_mobile_numbers).to eq ["07927829971", "07927829972"]
+    end
+  end
+
+  context 'filters diary entries based on speed' do
+    it "it returns the entry that best matches the reading speed and time available" do
+      diary = Diary.new
+      diary.add_entry("my entry")
+      diary.add_entry("more and more and more words")
+      expect(diary.diary_entries_based_on_speed(3, 2)).to eq ["more and more and more words"]
+    end
+
+    it "fails if there are no entries that match how much time i have" do
+      diary = Diary.new
+      diary.add_entry("my entry")
+      diary.add_entry("blah blah blah meh")
+      expect { diary.diary_entries_based_on_speed(3, 2) }.to raise_error "There are no suitable diary entries."
+    end
+  end
+
 end
