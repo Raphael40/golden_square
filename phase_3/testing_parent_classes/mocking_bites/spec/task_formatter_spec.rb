3c2b09adac8c161eb66de3b9b8c465c6009fbe92
Julian Festing <julfesting@gmail.com>
2023-04-22 12:16:35 +0100

    finished format task

--- phase_3/testing_parent_classes/mocking_bites/spec/task_formatter_spec.rb
+++ phase_3/testing_parent_classes/mocking_bites/spec/task_formatter_spec.rb
@@ -1,1 +1,17 @@
 require 'task_formatter'
+
+RSpec.describe 'reformats a task' do
+  it 'returns an incomplete task as a string with empty box' do
+    task = double :task, title: 'crazy task', complete?: false
+    task_formatter = TaskFormatter.new(task)
+
+    expect(task_formatter.format).to eq '- [ ] crazy task'
+  end
+
+  it 'returns a complete task as a string with an X in box' do
+    task = double :task, title: 'crazy task', complete?: true
+    task_formatter = TaskFormatter.new(task)
+
+    expect(task_formatter.format).to eq '- [x] crazy task'
+  end
+end
